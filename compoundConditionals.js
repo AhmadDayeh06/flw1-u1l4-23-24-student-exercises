0afb257a25d2f7e1bf75312ece9ff360d9547321
AhmadDayeh06 <141260835+AhmadDayeh06@users.noreply.github.com>
2023-10-25 23:22:42 +0000

    Initial commit for project.

--- compoundConditionals.js
+++ compoundConditionals.js
@@ -4,13 +4,26 @@
 
 
 // Change these values to test different scenarios
-let mood = "sad";        // Possible values: "happy", "sad", "excited", "bored"
-let timeOfDay = "night"; // Possible values: "morning", "afternoon", "evening", "night"
+let mood = "bored";        // Possible values: "happy", "sad", "excited", "bored"
+let timeOfDay = "afternoon"; // Possible values: "morning", "afternoon", "evening", "night"
 
 let recommendedMovie;
 
 // 1. Implement the recommendation logic using compound conditionals
 // - If the user is "tired" OR it's "night", recommend "Relaxing Ambient Film".
+if( mood === "tired" || timeOfDay === "night"){
+  recommendedMovie = "Relaxing Amient Film";
+}
+else if( mood === "excited" && timeOfDay !== "morning"){
+    recommendedMovie = "High-Octane Thriller";
+  }
+else if ((mood === "bored" && (timeOfDay === "afternoon") ) || (mood === "happy" && timeOfDay === "evening") ){
+    recommendedMovie = "Engaging Mystery Film";
+  }
+else
+{
+  recommendedMovie = "Popular Choice Film";
+}
 // - If the user is "excited" AND it's NOT "morning", recommend "High-Octane Thriller".
 // - If the user is "bored" AND it's "afternoon" OR "happy" AND it's "evening", recommend "Engaging Mystery Film".
 // - In all other cases, recommend "Popular Choice Film".
@@ -18,4 +31,4 @@ let recommendedMovie;
 
 
 // 2. Console log the recommended movie
-
+console.log(recommendedMovie);
